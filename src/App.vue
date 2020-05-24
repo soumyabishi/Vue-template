d3661817d30bb839d5677b378dbb7f054345ed1d
soumyabishi <soumyabishi@outlook.com>
2020-05-24 14:42:20 +0530

    Added Semantic ui and jquery

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  $import-google-fonts: true !default;
+  $font-url: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;800&display=swap' !default;
+  $font-name: 'Montserrat', sans-serif !default;
+  $font-family: $font-name, 'Montserrat', sans-serif !default;
+
+  $icons-font-path: '../node_modules/semantic-ui-sass/icons';
+  $flags-image-path: '../node_modules/semantic-ui-sass/images';
+  @import "~semantic-ui-sass/semantic-ui.scss";
+
+  #app {
+    font-family: 'Montserrat', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
